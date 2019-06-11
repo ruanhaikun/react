0a5610fd6805e2e1db19e58b0e68209d95b622d2
ruanhaikun <ruanhaikun@qq.com>
2019-06-11 18:19:20 +0800

    项目初始化

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react"
+
+// import React from "react"
+// const Component = React.Component                  ==  import React, { Component } from "react"
+
+class App extends Component {
+    render() {
+        return (
+            <div>hello world</div>
+        );
+    }
+}
+
+export default App;
